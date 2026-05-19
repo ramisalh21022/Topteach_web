6cf08ebc38c270c1876b22f24df23afcf0e6048d
ramisalh21022 <134399309+ramisalh21022@users.noreply.github.com>
2026-05-19 14:27:42 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
 
 export default function App() {
   return (
-   <BrowserRouter basename="/Topteach_web">
+    <HashRouter>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
-       
-             </Routes>
+      </Routes>
+
       <Footer />
-    </BrowserRouter basename="/Topteach_web">
+    </HashRouter>
   );
 }
