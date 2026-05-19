cc90c07d9f7f8e1ab4625e4ad57766803eb7d526
ramisalh21022 <134399309+ramisalh21022@users.noreply.github.com>
2026-05-19 08:28:51 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
-import About from "./pages/About.jsx";
-import Portfolio from "./pages/Portfolio.jsx";
-import Products from "./pages/Products.jsx";
-import Contact from "./pages/Contact.jsx";
-import Careers from "./pages/Careers.jsx";
-import FAQ from "./pages/FAQ.jsx";
+
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/careers" element={<Careers />} />
-        <Route path="/faq" element={<FAQ />} />
+       
              </Routes>
       <Footer />
     </BrowserRouter>
